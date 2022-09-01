a2dee9338bc55ae2c7f750343060f365058fbc29
hchitnis7 <harsh7chitnis@gmail.com>
2022-09-01 16:39:59 +0530

    first commit

--- VAAYU_CALCU.py
+++ VAAYU_CALCU.py
@@ -1,5 +1,9 @@
 key = "y"
 key_2 = "NO"
+key_3 = "NO"
+key_1 = "NEW"
+c = " "
+out = 0
 
 
 def calc_sum(x, y):
@@ -39,33 +43,41 @@ def is_prime(x):
         return True
 
 
-a, b = 0, 0
 while True:
-    if key_2 != "YES":
+    if key_1 == "NEW":
         a = int(input("Enter the 1st operand: \n"))
         print("Which operation would you like to perform?")
         c = input("addition: + \nsubtraction: - \ndivision: / \nmultiplication: * \nmodulus: mod \npercentage: % \nprime number check: prime \n")
-        b = int(input("Enter the 2nd operand: \n"))
-    else:
+    if key_2 == "SAME":
         print("Which operation would you like to perform?")
-        c = input("addition: + \nsubtraction: - \ndivision: / \nmultiplication: * \nmodulus: mod \npercentage: % \nprime number check: prime \n")
+        c = input(" meow 1 addition: + \nsubtraction: - \ndivision: / \nmultiplication: * \nmodulus: mod \npercentage: % \nprime number check: prime \n")
     c = c.lower()
+    if key_2 == "ANSWER":
+        a = out
+        print("Which operation would you like to perform?")
+        c = input(" meow 2 addition: + \nsubtraction: - \ndivision: / \nmultiplication: * \nmodulus: mod \npercentage: % \nprime number check: prime \n")
     if c == "+":
+        b = int(input("Enter the 2nd operand: \n"))
         out = calc_sum(a, b)
         print("The sum of the two numbers is: ", out)
     elif c == "-":
+        b = int(input("Enter the 2nd operand: \n"))
         out = calc_diff(a, b)
         print("The difference of the two numbers is: ", out)
     elif c == "*":
+        b = int(input("Enter the 2nd operand: \n"))
         out = calc_prod(a, b)
         print("The difference of the two numbers is: ", out)
     elif c == "/":
+        b = int(input("Enter the 2nd operand: \n"))
         out = calc_quo(a, b)
         print("The difference of the two numbers is: ", out)
     elif c == "mod":
+        b = int(input("Enter the 2nd operand: \n"))
         out = calc_mod(a, b)
         print("The difference of the two numbers is: ", out)
     elif c == "%":
+        b = int(input("Enter the 2nd operand: \n"))
         per = calc_percent(a, b)
         print(per)
     elif c == "prime":
@@ -74,17 +86,19 @@ while True:
             print(a, " is not a prime number")
         else:
             print(a, "is a prime number")
-        out = is_prime(b)
-        if out is False:
-            print(b, " is not a prime number")
-        else:
-            print(b, "is a prime number")
     else:
         print("invalid option entered, try again \n")
     key = input("Do you want to run again? y/n \n")
     if key == "y":
-        key_2 = input("if you want to run the calculator with different operation but same operands enter YES \n")
-        key_2 = key_2.upper()
+        key_1 = input("If you want to start the run a-fresh enter NEW/NO")
+        key_1 = key_1.upper()
+        if key_1 != "NEW":
+            key_2 = input("if you want to run the calculator with the answer from the last run enter ANSWER/NO \n")
+            if key_2 == "ANSWER":
+                key_2 = key_2.upper()
+            else:
+                key_3 = input("if you want to run the calculator with the same values and a different operation enter SAME/NO \n")
+                key_3 = key_3.upper()
         continue
     else:
         break
