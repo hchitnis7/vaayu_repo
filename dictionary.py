60d063c7fcca5e85d8c09d0fcfbe06c024bae60c
hchitnis7 <harsh7chitnis@gmail.com>
2022-09-02 17:54:52 +0530

    8th commit

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+students = {"name": ["Harsh", "meow"], "M_name": ["pallavi", "tata"], "L_name" : ["Chitnis", "bajaj"]}
+print(students)
+print(students.items())
+print(students.keys())
+students_1 = students.copy()
+students_1.pop("M_name")
+print(students_1)
+students_1.update(students)
+print(students_1)
+print(students_1.get("name"))
