162273a62341fe2f49947b5739158fdbf7c66493
hchitnis7 <harsh7chitnis@gmail.com>
2022-09-17 15:53:40 +0530

    working

--- colourdet_choice.py
+++ colourdet_choice.py
@@ -84,15 +84,7 @@ while True:
         low = np.array([0, 42, 0])
         high = np.array([255, 255, 255])
         mask_col = cv2.inRange(hsv_frame, low, high)
-        #contours, heirarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        #if len(contours) != 0:
-         #   for contour in contours:
-          #      if cv2.contourArea(contour) > 500:
-           #         x, y, w, h = cv2.boundingRect(contour)
-            #        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
         mask_ = cv2.bitwise_and(frame, frame, mask=mask_col)
-
         cv2.imshow("Frame", frame)
         cv2.imshow("white minus", mask_)
     if key == 27:
