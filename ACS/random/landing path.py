90b2c9e340e99dab7e6d116150ca0811556c580d
hchitnis7 <harsh7chitnis@gmail.com>
2022-12-05 13:05:43 +0530

    landing path pts

--- ACS/random/landing path.py
+++ ACS/random/landing path.py
@@ -1,6 +1,6 @@
 import plotter as ptpr
 import math
-chk_pts[]
+ckpts = []
 drp_h = int(input("enter the hieght at which the plane was dropped: "))
 calc_x = int(input("enter the calculated horizontal distance"))
 #assuming our landing zone is (0,0) drop point will be (calc_x, drp_h)
@@ -8,5 +8,18 @@ slp = drp_h/calc_x
 incln = math.atan(slp)
 incln = incln*(180/math.pi)
 a_speed = int(input("Enter the speed at which the pada is dropped"))
-as_y = a_speed*math.sin(a_speed)
-as_x = a_speed*math.cos((a_speed))
+as_y = -1*(a_speed*math.sin(a_speed))
+as_x = a_speed*math.cos(a_speed)
+c_alt = drp_h
+t = 1
+while(True):
+    ckpts.append(c_alt)
+    print(c_alt)
+    if (t * as_y)<0:
+        c_alt = drp_h + (t * as_y)
+    else:
+        c_alt = drp_h - (t * as_y)
+    if(c_alt<5):
+        break
+    t = t+1
+print(ckpts)
